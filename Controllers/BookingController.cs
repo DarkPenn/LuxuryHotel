aa613c09034ffa51e84424d88b4eb5a0ce3dfe66
DarkPenn <shadowpot915@gmail.com>
2026-07-29 21:03:27 +0700

    có chỉnh sửa db Layout part 1

---
+++ Controllers/BookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LuxuryHotel.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Models/KhachHang.cs
+++ Models/KhachHang.cs
@@ -16,5 +16,10 @@ namespace LuxuryHotel.Models
 
         public ICollection<DonDatPhong>? DonDatPhongs { get; set; }
         public ICollection<HoaDon>? HoaDons { get; set; }
+
+        //Bổ sung
+        public string Phone { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
