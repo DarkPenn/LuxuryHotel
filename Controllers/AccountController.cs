28fc701333079501406ab8ba3d116f5d493ba7c9
DarkPenn <shadowpot915@gmail.com>
2026-07-30 01:39:40 +0700

    RealFinalLayout

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using LuxuryHotel.Data;
+using LuxuryHotel.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,28 +14,27 @@ namespace LuxuryHotel.Controllers
             _context = context;
         }
 
-        public IActionResult CheckEmail()
-        {
-            return View();
-        }
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Login(string email, string password)
+        public IActionResult Login(string Email, string Password)
         {
-            var user = _context.KhachHangs
-                .FirstOrDefault(x => x.Email == email
-                                && x.Password == password);
+            var user = _context.KhachHangs.FirstOrDefault(x => x.Email == Email
+                                && x.Password == Password);
 
             if (user == null)
             {
+                ViewBag.Email = Email;
                 ViewBag.Error = "Sai email hoặc mật khẩu";
                 return View();
             }
 
-            HttpContext.Session.SetString("MaKH", user.MaKH);
+            HttpContext.Session.SetString("CustomerId", user.MaKH);
+            HttpContext.Session.SetString("CustomerName", user.TenKH);
+            HttpContext.Session.SetString("CustomerEmail", user.Email);
 
             return RedirectToAction("Index", "Home");
         }
@@ -42,12 +42,64 @@ namespace LuxuryHotel.Controllers
         public IActionResult Logout()
         {
             HttpContext.Session.Clear();
-            return RedirectToAction("Login");
+            return RedirectToAction("Index", "Home");
         }
 
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(KhachHang model, string ConfirmPassword)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState
+                    .SelectMany(x => x.Value.Errors.Select(e => $"{x.Key}: {e.ErrorMessage}"))
+                    .ToList();
+                return Content(string.Join("\n", errors));
+            }
+
+            if (model.Password != ConfirmPassword)
+            {
+                ModelState.AddModelError("", "Mật khẩu xác nhận không khớp.");
+                return View(model);
+            }
+
+            if (_context.KhachHangs.Any(x => x.Email == model.Email))
+            {
+                ModelState.AddModelError("Email", "Email đã tồn tại.");
+                return View(model);
+            }
+
+            if (_context.KhachHangs.Any(x => x.CCCD_CMND_KH == model.CCCD_CMND_KH))
+            {
+                ModelState.AddModelError("CCCD_CMND_KH", "CCCD/CMND đã được sử dụng.");
+                return View(model);
+            }
+
+            int count = _context.KhachHangs.Count() + 1;
+            model.MaKH = $"KH{count:000}";
+
+            model.MaKH = "KH" + Guid.NewGuid().ToString("N")[..8];
+
+            try
+            {
+                _context.KhachHangs.Add(model);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.ToString());
+            }
+
+            HttpContext.Session.SetString("CustomerId", model.MaKH);
+            HttpContext.Session.SetString("CustomerName", model.TenKH);
+            HttpContext.Session.SetString("CustomerEmail", model.Email);
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
