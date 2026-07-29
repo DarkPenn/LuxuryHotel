0d0a6b6d81e80da74a49f5ed12ef3e139ed6d6cd
DarkPenn <shadowpot915@gmail.com>
2026-07-29 11:19:36 +0700

    layout1

---
+++ Controllers/Account.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LuxuryHotel.Controllers
+{
+    public class Account : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
