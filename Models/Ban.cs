4877b58784faf75e388559c197d62007767086f8
Legion <Legion@DESKTOP-VPHEA6L>
2026-07-29 11:28:31 +0700

    data

---
+++ Models/Ban.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LuxuryHotel.Models
+{
+    public class Ban
+    {
+        [Key]
+        [StringLength(10)]
+        public string MaBan { get; set; } = null!;
+
+        [StringLength(50)]
+        public string TenBan { get; set; } = null!;
+
+        public ICollection<NhanVien>? NhanViens { get; set; }
+    }
+}
---
+++ Models/Phong.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LuxuryHotel.Models
+{
+    public class Phong
+    {
+        [Key]
+        [StringLength(10)]
+        public string MaPhong { get; set; } = null!;
+
+        [StringLength(50)]
+        public string LoaiPhong { get; set; } = null!;
+
+        public decimal Gia { get; set; }
+
+        public byte SoNguoi { get; set; }
+
+        public string DescriptionPhong { get; set; } = null!;
+
+        [StringLength(20)]
+        public string TrangThaiPhong { get; set; } = null!; // "Trống", "Có Khách", "Thiết Hại"
+
+        [StringLength(10)]
+        public string MaKS { get; set; } = null!;
+        [ForeignKey("MaKS")]
+        public KhachSan? KhachSan { get; set; }
+
+        public ICollection<BinhLuan>? BinhLuans { get; set; }
+        public ICollection<DichVuPhu>? DichVuPhus { get; set; }
+        public ICollection<DonDatPhong>? DonDatPhongs { get; set; }
+    }
+}
