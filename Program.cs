f338648044f719487d4859588704c1d2220b08e4
Legion <Legion@DESKTOP-VPHEA6L>
2026-07-29 20:03:45 +0700

    up

--- Program.cs
+++ Program.cs
@@ -3,11 +3,30 @@ using LuxuryHotel.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Đăng ký MVC
 builder.Services.AddControllersWithViews();
 
-// --- ĐĂNG KÝ DBCONTEXT TẠI ĐÂY ---
+// Đăng ký DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run(); // Chạy ứng dụng cơ bản
